fc1888af1ddeb7d99a5a9f8e8fca6e3623107ab5
Matthew <mdhays1995@gmail.com>
2016-05-22 22:22:37 -0500

    Finished this at work, adding to github for consistency.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string checkInputVal = "1121995";
+
+            Console.WriteLine("The numbers Mason, what do they mean?");
+
+            string userInput = Console.ReadLine();
+
+            if (userInput == checkInputVal)
+            {
+
+                Console.ForegroundColor = ConsoleColor.Blue;
+
+                Console.WriteLine("You stopped WWIII!");
+
+                Console.ReadLine();
+            }
+
+            else
+            {
+
+                Console.Beep();
+
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine("R.I.P");
+
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
